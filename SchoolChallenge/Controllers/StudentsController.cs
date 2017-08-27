4d7f1d272b09e93e0bb01f4485cfa3cc86fd8ecc
Anthony Campbell <anthony.campbell@alumni.msoe.edu>
2017-08-27 16:57:11 -0500

    - add student form - add methods to add, edit, and make new students - added methods to load one student and update a student in the database -added studentformview model to hold the instance of student

--- SchoolChallenge/Controllers/StudentsController.cs
+++ SchoolChallenge/Controllers/StudentsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
 using SchoolChallenge.Models;
+using SchoolChallenge.ViewModels;
 
 namespace SchoolChallenge.Controllers
 {
@@ -13,8 +14,67 @@ namespace SchoolChallenge.Controllers
     {
         public IActionResult Index()
         {
-            var studentsList = LoadStudents().ToList();
-            return View(studentsList);
+            var students = LoadStudents().ToList();
+            return View(students);
+        }
+
+        public IActionResult Add()
+        {
+            var student = new Student();
+            var viewModel = new StudentFormViewModel
+            {
+                Student = student
+            };
+
+            return View("StudentForm", viewModel);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var student = LoadStudent(id);
+
+            var viewModel = new StudentFormViewModel
+            {
+                Student = student
+            };
+
+            return View("StudentForm", viewModel);
+        }
+
+        public IActionResult Save(Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new StudentFormViewModel
+                {
+                    Student = student
+                };
+                return View("StudentForm", viewModel);
+            }
+
+            if (student.Id == 0)
+            {
+
+                var sqlStudentList = new List<Student>();
+                sqlStudentList = LoadStudents();
+
+                if (!sqlStudentList.Any(x => x.FirstName == student.FirstName && x.LastName == student.LastName &&
+                                             x.StudentNumber == student.StudentNumber))
+                {
+                    InsertStudent(student);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Students");
+                }
+
+            }
+            else
+            {
+                UpdateStudent(student);
+            }
+
+            return RedirectToAction("Index", "Students");
         }
 
         // method to load all students from the database
@@ -48,6 +108,89 @@ namespace SchoolChallenge.Controllers
                 }
             }
             return studentList;
+        }
+
+        public void InsertStudent(Student student)
+        {
+            using (var connection =
+                new SqlConnection(
+                    "Data Source=.\\SQLEXPRESS;Initial Catalog=SchoolChallengeDB;Trusted_Connection=True;MultipleActiveResultSets=true;")
+            )
+            {
+                string sql = "Insert into Students " +
+                             "(StudentNumber, FirstName, LastName, HasScholarship) " +
+                             "Values ('" +
+                             student.StudentNumber.ToString() + "','" +
+                             student.FirstName.ToString() + "','" +
+                             student.LastName.ToString() + "','" +
+                             student.HasScholarship.ToString() + "');";
+
+                var command = new SqlCommand();
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = sql;
+                command.Connection = connection;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        // update a student in the database
+        public void UpdateStudent(Student student)
+        {
+            using (var connection =
+                new SqlConnection(
+                    "Data Source=.\\SQLEXPRESS;Initial Catalog=SchoolChallengeDB;Trusted_Connection=True;MultipleActiveResultSets=true;")
+            )
+            {
+                string sql = "Update Students Set " +
+                             "StudentNumber='" + student.StudentNumber.ToString() + "'," +
+                             "FirstName='" + student.FirstName.ToString() + "'," +
+                             "LastName='" + student.LastName.ToString() + "'," +
+                             "HasScholarship='" + student.HasScholarship.ToString() + "' where id =" +
+                             student.Id.ToString();
+
+                var command = new SqlCommand();
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = sql;
+                command.Connection = connection;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        // load one student form the database
+        public Student LoadStudent(int id)
+        {
+            var student = new Student();
+
+            using (var connection =
+                new SqlConnection(
+                    "Data Source=.\\SQLEXPRESS;Initial Catalog=SchoolChallengeDB;Trusted_Connection=True;MultipleActiveResultSets=true;")
+            )
+            {
+                connection.Open();
+                string sql = "Select * from Students where Id =" + id.ToString();
+                using (var command = new SqlCommand(sql, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            student.Id = Int32.Parse(reader["Id"].ToString());
+                            student.StudentNumber = reader["StudentNumber"].ToString();
+                            student.FirstName = reader["FirstName"].ToString();
+                            student.LastName = reader["LastName"].ToString();
+                            student.HasScholarship = Boolean.Parse(reader["HasScholarship"].ToString());
+
+                        }
+                    }
+                }
+            }
+            return student;
         }
     }
 }
