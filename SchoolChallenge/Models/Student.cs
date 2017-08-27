0ee222111a8d0a28fa0898d9f46056e1a6700bcd
Anthony Campbell <anthony.campbell@alumni.msoe.edu>
2017-08-27 15:35:20 -0500

    add student and teacher model

---
+++ SchoolChallenge/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolChallenge.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Does this Student have a Scholarship?")]
+        public bool HasScholarship { get; set; }
+    }
+}
---
+++ SchoolChallenge/Models/Teacher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolChallenge.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+
+        public int? NumberOfStudents { get; set; }
+    }
+}
